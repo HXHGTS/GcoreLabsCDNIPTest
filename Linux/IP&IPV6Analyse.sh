158cfd6f41efe139e6b112dfd37da82eaf2a1819
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-13 00:30:52 +0800

    Update IP&IPV6Analyse.sh

--- Linux/IP&IPV6Analyse.sh
+++ Linux/IP&IPV6Analyse.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-mkdir -p /root/ping
-curl -sSL https://api.gcdn.co/cdn/public-ip-list > /root/ping/temp_ip.txt
-sed -i 's/", "/\n/g' /root/ping/temp_ip.txt
-sed -i "s/\"\], \"addresses_v6\": \[\"/\n/g" /root/ping/temp_ip.txt
-sed -i 's/{\"addresses\": \[\"//g' /root/ping/temp_ip.txt
-sed -i 's/\"\]}//g' /root/ping/temp_ip.txt
-cat /root/ping/temp_ip.txt | grep '/128' > /root/ping/ip6s.txt.temp
-sed -i 's/\/128//g' /root/ping/ip6s.txt.temp
-sort /root/ping/ip6s.txt.temp > /root/ping/ip6s.txt
-cat /root/ping/temp_ip.txt | grep '/32' > /root/ping/ips.txt.temp
-sed -i 's/\/32//g' /root/ping/ips.txt.temp
-sort /root/ping/ips.txt.temp > /root/ping/ips.txt
+mkdir -p /opt/ping
+curl -sSL https://api.gcdn.co/cdn/public-ip-list > /opt/ping/temp_ip.txt
+sed -i 's/", "/\n/g' /opt/ping/temp_ip.txt
+sed -i "s/\"\], \"addresses_v6\": \[\"/\n/g" /opt/ping/temp_ip.txt
+sed -i 's/{\"addresses\": \[\"//g' /opt/ping/temp_ip.txt
+sed -i 's/\"\]}//g' /opt/ping/temp_ip.txt
+cat /opt/ping/temp_ip.txt | grep '/128' > /opt/ping/ip6s.txt.temp
+sed -i 's/\/128//g' /opt/ping/ip6s.txt.temp
+sort /opt/ping/ip6s.txt.temp > /opt/ping/ip6s.txt
+cat /opt/ping/temp_ip.txt | grep '/32' > /opt/ping/ips.txt.temp
+sed -i 's/\/32//g' /opt/ping/ips.txt.temp
+sort /opt/ping/ips.txt.temp > /opt/ping/ips.txt
 sleep 1
-echo 'chcp 65001' > /root/ping/ping.bat
-echo 'del D:\/Tools\/Ping\/ping.log' >> /root/ping/ping.bat
-sed "s/^/ping -n 1 &/g" /root/ping/ips.txt >> /root/ping/ping.bat
-sed -i "s/$/& >> D:\/Tools\/Ping\/ping.log/g" /root/ping/ping.bat
-echo 'chcp 65001' > /root/ping/ping6.bat
-echo 'del D:\/Tools\/Ping\/ping6.log' >> /root/ping/ping.bat
-sed "s/^/ping -n 1 &/g" /root/ping/ip6s.txt >>  /root/ping/ping6.bat
-sed -i "s/$/& >> D:\/Tools\/Ping\/ping6.log/g" /root/ping/ping6.bat
-rm -f /root/ping/temp_ip.txt /root/ping/ips.txt.temp /root/ping/ip6s.txt.temp
+echo 'chcp 65001' > /opt/ping/ping.bat
+echo 'del D:\/Tools\/Ping\/ping.log' >> /opt/ping/ping.bat
+sed "s/^/ping -n 1 &/g" /opt/ping/ips.txt >> /opt/ping/ping.bat
+sed -i "s/$/& >> D:\/Tools\/Ping\/ping.log/g" /opt/ping/ping.bat
+echo 'chcp 65001' > /opt/ping/ping6.bat
+echo 'del D:\/Tools\/Ping\/ping6.log' >> /opt/ping/ping.bat
+sed "s/^/ping -n 1 &/g" /opt/ping/ip6s.txt >>  /opt/ping/ping6.bat
+sed -i "s/$/& >> D:\/Tools\/Ping\/ping6.log/g" /opt/ping/ping6.bat
+rm -f /opt/ping/temp_ip.txt /opt/ping/ips.txt.temp /opt/ping/ip6s.txt.temp
 exit
