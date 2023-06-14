503adc5d898f60048de20af25dd03e2a3f2942fa
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-14 17:49:47 +0800

    Update IPV6ResultsAnalyse.bat

--- Windows/IPV6ResultsAnalyse.bat
+++ Windows/IPV6ResultsAnalyse.bat
@@ -1,5 +1,5 @@
 @echo off
-type D:\Tools\Ping\ping6.log | findstr "Port is open" > D:\Tools\Ping\ping6.csv
+type D:\Tools\Ping\ping6.log | findstr "Port is open" | findstr /v "statistics" > D:\Tools\Ping\ping6.csv
 type D:\Tools\Ping\ping6.csv | findstr "time=1" > D:\Tools\Ping\ping6_100ms.csv
 type D:\Tools\Ping\ping6.csv | findstr "time=2" > D:\Tools\Ping\ping6_200ms.csv
 type D:\Tools\Ping\ping6.csv | findstr "time=3" > D:\Tools\Ping\ping6_300ms.csv
