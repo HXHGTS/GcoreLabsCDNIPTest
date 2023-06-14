1d092f7be590122f5decd61306a40ffb423a2d39
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-14 18:16:14 +0800

    Update IP&IPV6Analyse.sh

--- Linux/IP&IPV6Analyse.sh
+++ Linux/IP&IPV6Analyse.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cat /opt/ping/ping.log | grep 'Port is open' | grep -v 'statistics' > /opt/ping/ping.csv.temp
-awk -F ' ' '{print $2,$8 }' /opt/ping/ping.csv.temp | sed -e "s/:443\/tcp//g;s/time=//g;s/ms//g" > /opt/ping/ping.csv
+awk -F ' ' '{print $2,$8 }' /opt/ping/ping.csv.temp | sed -e "s/:443\/tcp//g;s/time=//g;s/ms//g" > /opt/ping/ping.csv.temp2
+sort -n -k 2 /opt/ping/ping.csv.temp2 > /opt/ping/ping.csv
 cat /opt/ping/ping.csv | grep 'time=1' > /opt/ping/ping_100ms.csv
 cat /opt/ping/ping.csv | grep 'time=2' > /opt/ping/ping_200ms.csv
 cat /opt/ping/ping.csv | grep 'time=3' > /opt/ping/ping_300ms.csv
