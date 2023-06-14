f15f23548c55f0e58c41e8ef0b3c25c797e466ee
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-14 17:35:14 +0800

    Update IPV4ResultsAnalyse.bat

--- Windows/IPV4ResultsAnalyse.bat
+++ Windows/IPV4ResultsAnalyse.bat
@@ -1,8 +1,6 @@
 @echo off
-type D:\Tools\Ping\ping.log | findstr "TTL" > D:\Tools\Ping\ping.csv
-type D:\Tools\Ping\ping.csv | findstr "=1" | findstr /v "TTL=1" > D:\Tools\Ping\ping_100ms.csv
-type D:\Tools\Ping\ping.csv | findstr "=2" | findstr /v "TTL=2"  > D:\Tools\Ping\ping_200ms.csv
-type D:\Tools\Ping\ping.csv | findstr /v "=1" | findstr /v "=2" | findstr /v "=4" | findstr /v "=5" > D:\Tools\Ping\ping_300ms.csv
-type D:\Tools\Ping\ping.csv | findstr "=4" | findstr /v "TTL=4"  > D:\Tools\Ping\ping_400ms.csv
-type D:\Tools\Ping\ping.csv | findstr "=5" | findstr /v "TTL=5"  > D:\Tools\Ping\ping_500ms.csv
+type D:\Tools\Ping\ping.log | findstr "Port is open" > D:\Tools\Ping\ping.csv
+type D:\Tools\Ping\ping.csv | findstr "=1" | findstr /v "time=1" > D:\Tools\Ping\ping_100ms.csv
+type D:\Tools\Ping\ping.csv | findstr "=2" | findstr /v "time=2"  > D:\Tools\Ping\ping_200ms.csv
+type D:\Tools\Ping\ping.csv | findstr "=2" | findstr /v "time=3"  > D:\Tools\Ping\ping_300ms.csv
 exit
