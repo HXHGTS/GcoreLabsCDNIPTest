887d65f7347bc667566a10a8333e988f2923a084
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-14 17:49:39 +0800

    Update IPV4ResultsAnalyse.bat

--- Windows/IPV4ResultsAnalyse.bat
+++ Windows/IPV4ResultsAnalyse.bat
@@ -1,5 +1,5 @@
 @echo off
-type D:\Tools\Ping\ping.log | findstr "Port is open" > D:\Tools\Ping\ping.csv
+type D:\Tools\Ping\ping.log | findstr "Port is open" | findstr /v "statistics" > D:\Tools\Ping\ping.csv
 type D:\Tools\Ping\ping.csv | findstr "time=1" > D:\Tools\Ping\ping_100ms.csv
 type D:\Tools\Ping\ping.csv | findstr "time=2" > D:\Tools\Ping\ping_200ms.csv
 type D:\Tools\Ping\ping.csv | findstr "time=3" > D:\Tools\Ping\ping_300ms.csv
