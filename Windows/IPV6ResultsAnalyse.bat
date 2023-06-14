187eec71dea0b94cbd10b8d350118833465998d2
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-14 17:39:03 +0800

    Update IPV6ResultsAnalyse.bat

--- Windows/IPV6ResultsAnalyse.bat
+++ Windows/IPV6ResultsAnalyse.bat
@@ -1,8 +1,6 @@
 @echo off
-type D:\Tools\Ping\ping6.log | findstr "TTL" > D:\Tools\Ping\ping6.csv
-type D:\Tools\Ping\ping6.csv | findstr "=1" | findstr /v "TTL=1"  > D:\Tools\Ping\ping6_100ms.csv
-type D:\Tools\Ping\ping6.csv | findstr "=2" | findstr /v "TTL=2"  > D:\Tools\Ping\ping6_200ms.csv
-type D:\Tools\Ping\ping6.csv | findstr "=3" | findstr /v "TTL=3"  > D:\Tools\Ping\ping6_300ms.csv
-type D:\Tools\Ping\ping6.csv | findstr "=4" | findstr /v "TTL=4"  > D:\Tools\Ping\ping6_400ms.csv
-type D:\Tools\Ping\ping6.csv | findstr "=5" | findstr /v "TTL=5"  > D:\Tools\Ping\ping6_500ms.csv
+type D:\Tools\Ping\ping6.log | findstr "Port is open" > D:\Tools\Ping\ping6.csv
+type D:\Tools\Ping\ping6.csv | findstr "time=1" > D:\Tools\Ping\ping6_100ms.csv
+type D:\Tools\Ping\ping6.csv | findstr "time=2" > D:\Tools\Ping\ping6_200ms.csv
+type D:\Tools\Ping\ping6.csv | findstr "time=3" > D:\Tools\Ping\ping6_300ms.csv
 exit
