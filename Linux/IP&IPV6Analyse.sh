826d422e92e85e4138317bcb7ba7fad6d2a87d70
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-14 17:33:49 +0800

    Update IP&IPV6Analyse.sh

--- Linux/IP&IPV6Analyse.sh
+++ Linux/IP&IPV6Analyse.sh
@@ -12,16 +12,17 @@ cat /opt/ping/temp_ip.txt | grep '/32' > /opt/ping/ips.txt.temp
 sed -i 's/\/32//g' /opt/ping/ips.txt.temp
 sort /opt/ping/ips.txt.temp > /opt/ping/ips.txt
 sleep 1
+rm -f /opt/ping/ping.bat /opt/ping/ping6.bat
 echo 'chcp 65001' > /opt/ping/ping.bat
 echo 'mkdir D:\Tools\Ping' >> /opt/ping/ping.bat
 echo 'del D:\Tools\Ping\ping.log' >> /opt/ping/ping.bat
 sed "s/^/ping -n 1 &/g" /opt/ping/ips.txt >> /opt/ping/ping.bat.temp
-sed -i "s/$/& >> D:\/Tools\/Ping\/ping.log/g" /opt/ping/ping.bat.temp
+sed -i "s/$/& >> D:\\\Tools\\\Ping\\\ping.log/g" /opt/ping/ping.bat.temp
 cat /opt/ping/ping.bat.temp >> /opt/ping/ping.bat
 echo 'chcp 65001' > /opt/ping/ping6.bat
 echo 'del D:\Tools\Ping\ping6.log' >> /opt/ping/ping6.bat
 sed "s/^/ping -n 1 &/g" /opt/ping/ip6s.txt >>  /opt/ping/ping6.bat.temp
-sed -i "s/$/& >> D:\/Tools\/Ping\/ping6.log/g" /opt/ping/ping6.bat.temp
+sed -i "s/$/& >> D:\\\Tools\\\Ping\\\ping6.log/g" /opt/ping/ping6.bat.temp
 cat /opt/ping/ping6.bat.temp >> /opt/ping/ping6.bat
 rm -f /opt/ping/temp_ip.txt /opt/ping/ips.txt.temp /opt/ping/ip6s.txt.temp /opt/ping/ping.bat.temp /opt/ping/ping6.bat.temp
 exit
