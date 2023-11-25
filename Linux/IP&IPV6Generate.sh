32bc572e33085afa4ec96d107dc46fb7ef47f5b1
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-11-25 17:20:23 +0800

    Update IP&IPV6Generate.sh

--- Linux/IP&IPV6Generate.sh
+++ Linux/IP&IPV6Generate.sh
@@ -5,12 +5,12 @@ sed -i 's/", "/\n/g' /opt/ping/temp_ip.txt
 sed -i "s/\"\], \"addresses_v6\": \[\"/\n/g" /opt/ping/temp_ip.txt
 sed -i 's/{\"addresses\": \[\"//g' /opt/ping/temp_ip.txt
 sed -i 's/\"\]}//g' /opt/ping/temp_ip.txt
-cat /opt/ping/temp_ip.txt | grep '/128' > /opt/ping/ip6s.txt.temp
-sed -i 's/\/128//g' /opt/ping/ip6s.txt.temp
-sort /opt/ping/ip6s.txt.temp > /opt/ping/ip6s.txt
-cat /opt/ping/temp_ip.txt | grep '/32' > /opt/ping/ips.txt.temp
-sed -i 's/\/32//g' /opt/ping/ips.txt.temp
-sort /opt/ping/ips.txt.temp > /opt/ping/ips.txt
+cat /opt/ping/temp_ip.txt | grep '/128' > /opt/ping/ip6s_cst.txt
+sed -i 's/\/128//g' /opt/ping/ip6s_cst.txt
+sort /opt/ping/ip6s_cst.txt > /opt/ping/ip6s.txt
+cat /opt/ping/temp_ip.txt | grep '/32' > /opt/ping/ips_cst.txt
+sed -i 's/\/32//g' /opt/ping/ips_cst.txt
+sort /opt/ping/ips_cst.txt > /opt/ping/ips.txt
 sleep 1
-rm -f /opt/ping/temp_ip.txt /opt/ping/ips.txt.temp /opt/ping/ip6s.txt.temp
+rm -f /opt/ping/temp_ip.txt
 exit
