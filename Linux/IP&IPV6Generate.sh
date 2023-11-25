8b20eaf0774c4bef2fad1b83d0c462348f3a1d0f
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-11-25 16:21:31 +0800

    Update IP&IPV6Generate.sh

--- Linux/IP&IPV6Generate.sh
+++ Linux/IP&IPV6Generate.sh
@@ -17,12 +17,12 @@ echo 'chcp 65001' > /opt/ping/ping.bat
 echo 'mkdir D:\Tools\Ping' >> /opt/ping/ping.bat
 echo 'del D:\Tools\Ping\ping.log' >> /opt/ping/ping.bat
 sed "s/^/psping -n 1 &/g" /opt/ping/ips.txt >> /opt/ping/ping.bat.temp
-sed -i "s/$/& 443 >> D:\\\Tools\\\Ping\\\ping.log/g" /opt/ping/ping.bat.temp
+sed -i "s/$/&:443 >> D:\\\Tools\\\Ping\\\ping.log/g" /opt/ping/ping.bat.temp
 cat /opt/ping/ping.bat.temp >> /opt/ping/ping.bat
 echo 'chcp 65001' > /opt/ping/ping6.bat
 echo 'del D:\Tools\Ping\ping6.log' >> /opt/ping/ping6.bat
 sed "s/^/tcping -n 1 &/g" /opt/ping/ip6s.txt >>  /opt/ping/ping6.bat.temp
-sed -i "s/$/& 443 >> D:\\\Tools\\\Ping\\\ping6.log/g" /opt/ping/ping6.bat.temp
+sed -i "s/$/&:443 >> D:\\\Tools\\\Ping\\\ping6.log/g" /opt/ping/ping6.bat.temp
 cat /opt/ping/ping6.bat.temp >> /opt/ping/ping6.bat
 rm -f /opt/ping/temp_ip.txt /opt/ping/ips.txt.temp /opt/ping/ip6s.txt.temp /opt/ping/ping.bat.temp /opt/ping/ping6.bat.temp
 exit
