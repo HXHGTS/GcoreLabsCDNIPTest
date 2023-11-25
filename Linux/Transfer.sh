c8ce8c088c5ff2e432928a70f2a65e651f6b148a
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-11-25 16:13:52 +0800

    Update Transfer.sh

--- Linux/Transfer.sh
+++ Linux/Transfer.sh
@@ -2,9 +2,9 @@
 
 cd /opt/ping
 
-for file in *.txt
+for file in *.{txt,bat}
 do
   sed -i.bak 's/$/\r/' "$file"
 done
 
-echo "All *.txt files in ping folder have been converted to windows line endings."
+echo "All *.txt and *.bat files in ping folder have been converted to windows line endings."
